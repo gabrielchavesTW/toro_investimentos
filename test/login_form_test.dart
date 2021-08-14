b43d13529a11122846b5f6019b96df1e495ff3b8
Gabriel Chaves <gabriel.chaves@thoughtworks.com>
2021-08-14 16:19:03 -0300

    renomeando os testes para o componente de login_form

---
+++ test/login_form_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:toro_investimentos/src/ui/login/login_form.dart';
+
+void main() {
+  Future<void> setup(WidgetTester tester) async {
+    await tester.pumpWidget(Container(child: LoginForm(),));
+  }
+
+  testWidgets('Login form should have email and senha inputs',
+      (WidgetTester tester) async {
+    await setup(tester);
+
+    expect(find.text('E-mail ou CPF:'), findsOneWidget);
+    expect(find.text('Senha:'), findsOneWidget);
+  });
+
+  testWidgets('Login form should have button with entrar text',
+      (WidgetTester tester) async {
+    await setup(tester);
+
+    expect(find.text('ENTRAR'), findsOneWidget);
+  });
+}
